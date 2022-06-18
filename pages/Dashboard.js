c1afa0bd0358e1ca7d40430c0d8181dcc8c09505
Asghar Ali Mowlana <alimowlana24@gmail.com>
2022-06-18 22:49:42 +0400

    Retrieved tokens details using an api from the sanity database

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -4,6 +4,9 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Sidebar from "../components/Sidebar";
 
+// Thirdweb
+// https://thirdweb.com/dashboard
+
 const Dashboard = ({ address }) => {
   return (
     <Wrapper>
