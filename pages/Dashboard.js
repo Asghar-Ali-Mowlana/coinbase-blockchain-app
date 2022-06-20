24c95ce7f777d2d364a8fafd1c49f8f8600a7057
Asghar Ali Mowlana <alimowlana24@gmail.com>
2022-06-20 11:42:30 +0400

    Connected sanity database and thirdweb

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { ethers } from "ethers";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Sidebar from "../components/Sidebar";
@@ -7,12 +9,47 @@ import Sidebar from "../components/Sidebar";
 // Thirdweb
 // https://thirdweb.com/dashboard
 
+const PRIVATE_KEY = process.env.NEXT_PUBLIC_METAMASK_KEY;
+
+const sdk = ThirdwebSDK.fromPrivateKey(PRIVATE_KEY, "rinkeby");
+
+/*const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.NEXT_PUBLIC_METAMASK_KEY,
+    ethers.getDefaultProvider("https://rinkeby.infura.io/v3/")
+  )
+);*/
+
 const Dashboard = ({ address }) => {
+  const [sanityTokens, setSanityTokens] = useState([]);
+  const [thirdWebTokens, setThirdWebTokens] = useState([]);
+
+  useEffect(() => {
+    const getSanityAndThirdWebTokens = async () => {
+      try {
+        const coins = await fetch(
+          "https://dvb77qge.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%27coins%27%5D%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
+        );
+        const sanityTokens = (await coins.json()).result;
+        setSanityTokens(sanityTokens);
+        setThirdWebTokens(
+          sanityTokens.map((token) => sdk.getToken(token.contractAddress))
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getSanityAndThirdWebTokens();
+  }, []);
+
+  console.log("Sanity", sanityTokens);
+  console.log("ThirdWeb", thirdWebTokens);
+
   return (
     <Wrapper>
       <Sidebar />
       <MainContainer>
-        <Header walletAddress={address}  />
+        <Header walletAddress={address} />
         <Main />
       </MainContainer>
     </Wrapper>
